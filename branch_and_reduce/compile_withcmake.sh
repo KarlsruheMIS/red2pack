ba99a7adcf3156b22fc5713bef724055d5b14369
jabo17 <jannick.borowitz@gmail.com>
2023-06-30 13:46:06 +0200

    small changes

--- branch_and_reduce/compile_withcmake.sh
+++ branch_and_reduce/compile_withcmake.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 NCORES=4
-unamestr=`uname`
+unamestr=$(uname)
 if [[ "$unamestr" == "Linux" ]]; then
-        NCORES=`grep -c ^processor /proc/cpuinfo`
+  NCORES=$(grep -c ^processor /proc/cpuinfo)
 fi
 
 if [[ "$unamestr" == "Darwin" ]]; then
-        NCORES=`sysctl -n hw.ncpu`
+  NCORES=$(sysctl -n hw.ncpu)
 fi
 
 rm -rf deploy
 rm -rf build
 mkdir build
 cd build
-cmake ../ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-13 -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-13 -DCMAKE_BUILD_TYPE=Release -DCMAKE_BUILD_TYPE=True
+cmake ../ -DCMAKE_C_COMPILER=/usr/local/bin/gcc-13 -DCMAKE_CXX_COMPILER=/usr/local/bin/g++-13 -DCMAKE_BUILD_TYPE=Release -DCMAKE_ENABLE_TESTING=False
 make -j $NCORES
 cd ..
 
