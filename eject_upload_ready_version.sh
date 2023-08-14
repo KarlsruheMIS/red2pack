d4b8c1d6a16468bfe9fad7469405895f07d83748
jabo17 <jannick.borowitz@gmail.com>
2023-08-14 09:15:12 +0200

    clean up and out of the box experiment

---
+++ eject_upload_ready_version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+trap "echo exiting...; exit" SIGHUP SIGINT SIGTERM
+echo "This transforms this repository to an upload ready version and cannot be undone. If you want to proceed, hit a key. Otherwise exit with Ctrl+C."
+read var
+
+echo "Preparing upload ready version."
+
+git stash
+git submodule update --init --recursive
+
+if [ ! -d branch_and_reduce/extern/KaMIS ]; then
+  echo "KaMIS not downloaded!"
+  exit 1
+fi
+
+if [ ! -d branch_and_reduce/graphs/Gene2Pack ]; then
+  echo "Gene2Pack not downloaded!"
+  exit 1
+fi
+
+if [ ! -d branch_and_reduce/graphs/Approximate2Packing ]; then
+  echo "Approximate2Packing not downloaded!"
+  exit 1
+fi
+
+rm data_table.py
+rm eval_results.py
+rm gen_algo.py
+rm gen_jobs*
